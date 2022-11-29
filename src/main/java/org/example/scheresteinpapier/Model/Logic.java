da0e08db2a8ab0364f7cfe9aa34116f7728b599e
dkupinic <dkupinic@htl-steyr.ac.at>
2022-11-29 14:06:03 +0100

    added Player class added Action enum added Logic class addes styles css created packages

---
+++ src/main/java/org/example/scheresteinpapier/Model/Logic.java
@@ -0,0 +1,9 @@
+package org.example.scheresteinpapier.Model;
+
+public class Logic {
+    public static void checkWin(Player player1, Player player2) {
+        if (player1.getAction() == Action.NONE && player2.getAction() == Action.NONE) {
+            // Label: Nichts ausgewählt
+        }
+    }
+}
