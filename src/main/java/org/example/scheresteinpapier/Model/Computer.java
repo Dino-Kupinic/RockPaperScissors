ba5fad29ebdea46c643606a524f1fdc69264c966
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-12-02 21:28:50 +0100

    split getComputerChoice() into 3 functions and commented them

--- src/main/java/org/example/scheresteinpapier/Model/Computer.java
+++ src/main/java/org/example/scheresteinpapier/Model/Computer.java
@@ -2,20 +2,31 @@ package org.example.scheresteinpapier.Model;
 
 public class Computer {
     public static Player getComputerChoice() {
-        double choice = Math.floor(Math.random() * 3 + 1);
-        Action computerAction = Action.NONE;
-        switch ((int) choice) {
-            case 1:
-                computerAction = Action.ROCK;
-                break;
-            case 2:
-                computerAction = Action.PAPER;
-                break;
-            case 3:
-                computerAction = Action.SCISSOR;
-                break;
-        }
-        System.out.println(computerAction);
+        Action computerAction = getComputerAction();
         return new Player(0, computerAction);
+    }
+
+    /**
+     * Function to generate value between 1 and 3 which gets evaluated into an action
+     * @return a double value (1.0, 2.0 or 3.0)
+     */
+    private static double generateRandomNumber() {
+        return Math.floor(Math.random() * 3 + 1);
+    }
+
+    /**
+     * Function to evaluate number into a valid action
+     * @return Action which will be evaluated in order to decide who won the game
+     */
+    private static Action getComputerAction() {
+        switch ((int) generateRandomNumber()) {
+            case 1:
+                return Action.ROCK;
+            case 2:
+                return Action.PAPER;
+            case 3:
+                return Action.SCISSOR;
+        }
+        return Action.NONE; // TODO: Add Exception
     }
 }
