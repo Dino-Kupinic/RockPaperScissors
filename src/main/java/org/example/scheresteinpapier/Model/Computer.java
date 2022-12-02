d4b6acdf1838662884237a31d88bac9cd44e7593
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-12-02 21:20:21 +0100

    finished getComputerChoice()

--- src/main/java/org/example/scheresteinpapier/Model/Computer.java
+++ src/main/java/org/example/scheresteinpapier/Model/Computer.java
@@ -1,7 +1,21 @@
 package org.example.scheresteinpapier.Model;
 
 public class Computer {
-    public static Player generateComputerChoice() {
-        return new Player();
+    public static Player getComputerChoice() {
+        double choice = Math.floor(Math.random() * 3 + 1);
+        Action computerAction = Action.NONE;
+        switch ((int) choice) {
+            case 1:
+                computerAction = Action.ROCK;
+                break;
+            case 2:
+                computerAction = Action.PAPER;
+                break;
+            case 3:
+                computerAction = Action.SCISSOR;
+                break;
+        }
+        System.out.println(computerAction);
+        return new Player(0, computerAction);
     }
 }
