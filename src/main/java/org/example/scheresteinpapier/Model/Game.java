f0bb6f72393d3d5e5aba727bae657963078d967e
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-12-03 12:34:02 +0100

    minor changes

--- src/main/java/org/example/scheresteinpapier/Model/Game.java
+++ src/main/java/org/example/scheresteinpapier/Model/Game.java
@@ -1,32 +1,5 @@
 package org.example.scheresteinpapier.Model;
 
 public class Game {
-    private boolean playerWon;
-    private int rounds;
 
-    private int roundsWon;
-
-
-    public boolean isPlayerWon() {
-        return playerWon;
-    }
-    public void setPlayerWon(boolean playerWon) {
-        this.playerWon = playerWon;
-    }
-
-
-    public int getRounds() {
-        return rounds;
-    }
-    public void setRounds(int rounds) {
-        this.rounds = rounds;
-    }
-
-
-    public int getRoundsWon() {
-        return roundsWon;
-    }
-    public void setRoundsWon(int roundsWon) {
-        this.roundsWon = roundsWon;
-    }
 }
